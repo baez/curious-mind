2fbca5adf60259e157009ce5b80f053ab125ac59
root <GB Karim>
2025-02-15 07:02:11 -0800

    Renamed proj and solution.

---
+++ random-qs/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using models;
+using problems;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("===== Test from random-qs -- hello :D =====");
+
+        if (args.Length == 0)
+        {
+            throw new ArgumentException("Error: Need command line argument for filename containing data -- E.g. dotnet run CarData.txt");
+        }
+
+        TestCarQueries(args[0]);
+    }
+
+    public static void TestCarQueries(string filename) 
+    {    
+        var lines = File.ReadLines(filename).ToArray();
+
+        var cars = new List<Car>(); 
+        Console.WriteLine($"Reading data from {filename}");
+
+        foreach (string line in lines) 
+        { 
+            string[] strArr = line.Split(' '); 
+            cars.Add(new Car {  
+                Brand = strArr[0],  
+                Model = strArr[1],  
+                Price = int.Parse(strArr[2])  
+                });
+            
+            Console.WriteLine($"{strArr[0]}, {strArr[1]}, {strArr[2]}"); 
+        }
+
+        var carQuery = new CarPriceQuery();
+
+        var mostExpensiveCar = carQuery.MostExpensiveCar(cars); 
+        Console.WriteLine($"The most expensive car is {mostExpensiveCar.Brand} {mostExpensiveCar.Model}"); 
+        var cheapestCar = carQuery.CheapestCar(cars); 
+
+        Console.WriteLine($"The cheapest car is {cheapestCar.Brand} {cheapestCar.Model}"); 
+        Console.WriteLine($"The average price of all cars is {carQuery.AveragePriceOfCars(cars)}"); 
+
+        foreach (var res in carQuery.MostExpensiveModelForEachBrand(cars)) 
+        { 
+            Console.WriteLine($"The most expensive model for brand {res.Key} is {res.Value.Model} having price {res.Value.Price}"); 
+        } 
+    }  
+}
