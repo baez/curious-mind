4a66ba2d179542ae0b0805e5760e80694408992e
root <GB Karim>
2025-02-15 07:05:25 -0800

    Renamed proj and solution.

--- ConsoleApp2/Models/Car.cs
+++
@@ -1,9 +0,0 @@
-    namespace models
-    {
-        public class Car
-        { 
-            public string Brand { get; set; } 
-            public string Model { get; set; } 
-            public int Price { get; set; } 
-        } 
-    }
